879a678d221b464a98a3166afb97c10d99470486
Jatin-kv <120176047+Jatin-kv@users.noreply.github.com>
2023-01-31 20:17:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+let evaluatedText=document.getElementById("evaluatedText");
+let wordCount=document.getElementById("wordCount");
+
+evaluatedText.addEventListener("input",calculateWord)
+
+	function calculateWord() {
+		let word = evaluatedText.value.trim();
+		if (word!=""){
+			wordCount.innerHTML = word.split(" ").length;;
+		}
+		else{
+			wordCount.innerHTML = "0";
+		}
+	}
